e6160e8a47f85d972eb3490ce720a0fbaca30112
Larry Lu <pudding850806@gmail.com>
2018-04-16 09:59:59 +0800

    Add atomic example

---
+++ add_many_times_with_atomic/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+func main() {
+	var a int32 = 0
+	times := 10000
+	c := make(chan bool)
+
+	for i := 0; i < times; i++ {
+		go func() {
+			atomic.AddInt32(&a, 1)
+			c <- true
+		}()
+	}
+
+	for i := 0; i < times; i++ {
+		<-c
+	}
+	fmt.Printf("a = %d\n", a)
+}
